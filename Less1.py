ffc3d9481fe7560f9657feb7b8dee8adcefddf1d
Sahin <10_02@mail.ru>
2022-02-10 16:12:32 +0300

    Info_os

--- Less1.py
+++ Less1.py
@@ -1,7 +1,18 @@
-#echo "# MyLessons" >> README.md
+#echo "# MyLessons" >> README.me
+
 #git init
 #git add README.md
 #git commit -m "first commit"
 #git branch -M main
 #git remote add origin https://github.com/Sahinov/MyLessons.git
 #git push -u origin main
+import platform
+import sys
+
+info = 'OS info is \n{}\n\nPython Version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture()
+)
+print(info)
+
+with open('info_os.txt', 'w') as ff:
+    ff.write(info)
